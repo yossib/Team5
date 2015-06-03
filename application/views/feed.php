fc84a7be09c9deafd43352004c2e291bd6f5abd5
yossib <yossibn@gmail.com>
2015-06-03 22:44:14 +0300

    Feed rendering of posts, comments and ability to post new post and comments

--- application/views/feed.php
+++ application/views/feed.php
@@ -1,6 +1,10 @@
 <script type="text/javascript">
+  var usedId = 1;
   $(document).ready(get_latest_posts());
-
+  $(document).ready(function(){
+    $("#newPost").on("click",".makePost",makePost);
+    $(document).on("click",".makeNewComment", makeComment);
+  })
   function get_latest_posts(){
     console.log('here');
     var url = "http://localhost/feed/getrecentposts"
@@ -22,25 +26,79 @@
 
   function render_posts(data){
     $.each(data, function(k, entry){
-      console.log(entry);
-      var post = $(postTemplate)
-      $(".panel-title", post).html(entry.name);
-      $(".panel-body", post).html(entry.content);
+      var post = $(postTemplate);
+      post.data(entry);
+      $(".panel-title", post).html(entry.created_at + ", By: " + entry.name);
+      $(".panel-body-content", post).html(entry.content);
+      $.each(entry.comments, function(k, c){
+        var comment = $(commentTemplate);
+        $(".panel-title", comment).html("By: " + c.name);
+        $(".panel-body", comment).html(c.content);
+        $(".panel-comments",post).append(comment);
+      })
       $("#feed").prepend(post);
     })
 
   }
 
-  var postTemplate = '<div class="panel panel-primary">' +
+  var newCommentTemplate = '<div class="input-group .newComment"> ' +
+    '<span class="input-group-btn makeNewComment" style="vertical-align: top"> ' +
+    '<button class="btn btn-default" type="button">+</button> ' +
+    '</span> ' +
+    '<textarea class="form-control newCommentContent" rows="3">comment</textarea> ' +
+    '</div>';
+
+  var postTemplate = '<div class="panel panel-primary post">' +
     '<div class="panel-heading"> ' +
     '<h3 class="panel-title">Panel title</h3> ' +
     '</div> ' +
-    '<div class="panel-body">Panel content </div> ' +
+    '<div class="panel-body">' +
+    '<div class="panel-body-content">Panel content</div>' +
+    '<div class="panel-comments"></div>' +
+    '<div>' +  newCommentTemplate + '</div>' +
+    '</div></div> ' +
     '</div>';
+
+  var commentTemplate = '<div class="panel panel-info"> ' +
+    '<div class="panel-heading"> ' +
+    '<h3 class="panel-title">Panel title</h3> ' +
+    '</div> ' +
+    '<div class="panel-body">' +
+    '' +
+    '</div> ' +
+    '</div>'
+
+  function makePost(){
+    var content = $("#newPostContent").html();
+    var url = "http://localhost/feed/savepost";
+    $.post(url , { userId : usedId, content : content }, function(data){
+      console.log(data);
+    } , "json");
+  }
+
+  function makeComment(){
+    console.log('here');
+    var post = $(this).closest(".post");
+    var content = $(".newCommentContent", post).html();
+    var url = "http://localhost/feed/savecomment";
+    postId = post.data().post_id;
+    $.post(url , { userId : usedId, "postId" : postId, content : content }, function(data){
+      console.log(data);
+    } , "json");
+  }
+
 
 
 </script>
 <h1>Somoto Feed By Team5!</h1>
+<div class="input-group" id="newPost">
+  <span class="input-group-btn makePost" style="vertical-align: top">
+    <button class="btn btn-default" type="button">+</button>
+  </span>
+  <textarea class="form-control" rows="5" id="newPostContent">Make a new post</textarea>
+</div>
+
+<br/><br/>
 <div class="row" id="feed">
   <div class="panel panel-primary">
     <div class="panel-heading">
