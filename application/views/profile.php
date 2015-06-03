cc00839f63541ad20918e1d8b930ee44a5d65ef7
Umitay <umitay.turdykulov@gmail.com>
2015-06-03 22:34:45 +0300

    added user tpl

--- application/views/profile.php
+++
@@ -1,3 +0,0 @@
-<?php
-?>
-This is a motherfucking profile page!!
---
+++ application/views/profile_view.php
@@ -0,0 +1,39 @@
+<div class="row">
+	  <div class="col-md-8">
+		<form>
+		  <div class="form-group">
+			<label for="first_name">Name</label>
+			<div><?=$first_name?></div>
+		  </div>
+		   <div class="form-group">
+			<label for="last_name">Last name</label>
+			<div><?=$last_name?></div>
+		  </div>
+		   <div class="form-group">
+			<label for="birthday">Birthday</label>
+			<div><?=$birthday?></div>
+		  </div>
+		   <div class="form-group">
+			<label for="position">Position</label>
+			<div><?=$position?></div>
+		  </div>
+		   <div class="form-group">
+			<label for="work_start_date">Work start date</label>
+			<div><?=$work_start_date?></div>
+		  </div>
+		  <div class="form-group">
+			<label for="password">Email address</label>
+			<div><?=$email?></div>
+		  </div>
+		 
+		  <div class="form-group">
+			<label for="avatar">Avatar Url</label>
+			<div><?=$avatar?></div>
+		  </div>
+		   <div class="form-group">
+			<label for="description">Description</label>
+			<p><?=$description?></p>
+		  </div>
+		  <button type="submit" class="btn btn-default">Submit</button>
+		</form>
+			  
