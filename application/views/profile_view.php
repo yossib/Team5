d8db47485ae5252a895911f2391644cad3c93550
yossib <yossibn@gmail.com>
2015-06-04 08:01:33 +0300

    UI improvements

--- application/views/profile_view.php
+++ application/views/profile_view.php
@@ -8,34 +8,35 @@
   }
 </style>
 <div class="row">
-	  <div class="col-md-8">
-		<form>
+	  <div class="col-md-8" style="padding-left:500px; text-align:left; color: #fff">
+			<img src="<?=$avatar?>" style="height: 150px; width: 150px; float: left; margin-left: -250px" />
+			<form>
 		 <div class="form-group">
-			<div><img src="<?=$avatar?>" />&nbsp;&nbsp;&nbsp;&nbsp;<span class="username"><?=$first_name?> <?=$last_name?></span></div>
-       <?php if($edit){ ?> <a href="/profile/edit/<?=$id?>">Edit details</a> <?php }?>
+			<div>
+				<span class="username"><?=$first_name?> <?=$last_name?></span></div>
+       <?php if($edit){ ?> <div class="value">
+				 <h4><span class="label label-primary" onClick="location.href='/profile/edit/<?=$id?>'" style="cursor: pointer">Edit details</span></h4></div> <?php }?>
 		  </div>
       <br><br>
 		   <div class="form-group">
 			<label for="birthday">Birthday</label>
-			<div class="value"><?=$birthday?></div>
+			<div class="value"><?= ($birthday == "0000-00-00") ? "Not entered yet" : $birthday ?></div>
 		  </div>
 		   <div class="form-group">
 			<label for="position">Position</label>
-			<div class="value"><?=$position?></div>
+			<div class="value"><?=($position == "")? "Not entered yet" : $position ?></div>
 		  </div>
 		   <div class="form-group">
 			<label for="work_start_date">Work start date</label>
-			<div class="value"><?=$work_start_date?></div>
+			<div class="value"><?= ($work_start_date == "0000-00-00") ? "Not entered yet" : $work_start_date?></div>
 		  </div>
 		  <div class="form-group">
 			<label for="password">Email address</label>
 			<div class="value"><?=$email?></div>
 		  </div>
-		 
-		 
 		   <div class="form-group">
 			<label for="description">Description</label>
-			<p><?=$description?></p>
+			 <div class="value"><?=($description == "")? "Not entered yet" : $description ?></div>
 		  </div>
 		</form>
 			  
